a72ee468e4f6fecd7fddf35c2edd3d5252f2073d
Valdemar <vsglazov@gmail.com>
2021-06-19 23:00:51 +0300

    Установлено подключение к серверу mySQL

--- server/index.js
+++ server/index.js
@@ -1,28 +1,50 @@
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'db_damyen_1',
+  password: '',
+});
+
+// тестирование подключения
+connection.connect(err => {
+  if (err) {
+    return console.error('Ошибка: ' + err.message);
+  }
+  console.log('Подключение к серверу MySQL успешно установлено');
+});
+// закрытие подключения
+connection.end(function(err) {
+  if (err) {
+    return console.log('Ошибка: ' + err.message);
+  }
+  console.log('Подключение закрыто');
+});
+
 const consola = require('consola');
 const { Nuxt, Builder } = require('nuxt');
 const app = require('./app');
 
-// Import and Set Nuxt.js options
 const config = require('../nuxt.config.js');
-config.dev = process.env.NODE_ENV !== 'production';
+config.dev = !(process.env.NODE_ENV === 'production');
 
 async function start() {
-  // Init Nuxt.js
   const nuxt = new Nuxt(config);
 
-  const { host, port } = nuxt.options.server;
+  const {
+    host = process.env.HOST || '127.0.0.1',
+    port = process.env.PORT || 3000,
+  } = nuxt.options.server;
 
   await nuxt.ready();
-  // Build only in dev mode
+
   if (config.dev) {
     const builder = new Builder(nuxt);
     await builder.build();
   }
 
-  // Give nuxt middleware to express
   app.use(nuxt.render);
 
-  // Listen the server
   app.listen(port, host, () => {
     consola.ready({
       message: `Server listening on http://${host}:${port}`,
