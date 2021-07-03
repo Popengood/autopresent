faa7a630b35842e8bf1804536ff89ab107d44af6
Valdemar <vsglazov@gmail.com>
2021-07-03 20:03:49 +0300

    Добавлена папка addinform вместе с файлами

---
+++ pages/addinform/novetly.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="content">
+    <h1>Новинки автолитературы по ремонту и эксплуатации автомобилей.</h1>
+    <ListBookItem v-for="book in books.books" :key="book.id" :book="book" />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      books: [],
+    }
+  },
+  async fetch() {
+    try {
+      this.books = await this.$axios.$get('/api/addinform/novetly');
+    } catch (e) {
+      console.error(e);
+      throw e;
+    }
+  },
+  components: {
+    ListBookItem: () => import('~/components/books/ListBookItem'),
+  }
+}
+</script>
