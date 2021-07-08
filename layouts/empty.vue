5d229017f7c778acf8dde6e4eb7ade8b7870160f
Valdemar <vsglazov@gmail.com>
2021-07-08 13:01:40 +0300

    Добавлена страница error

---
+++ layouts/empty.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="flex container container-error">
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
--- layouts/error.vue
+++ layouts/error.vue
@@ -18,7 +18,7 @@
 
 <script>
 export default {
-	layout: 'notaside',
+	layout: 'empty',
 };
 </script>
 
