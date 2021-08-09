0fe6a2d61d19a2f74ab9201bb1b1909b59379fae
Valdemar <vsglazov@gmail.com>
2021-08-09 11:57:51 +0300

    Создан подвал сайта

--- components/footer/Footer.vue
+++ components/footer/Footer.vue
@@ -1,7 +1,28 @@
 <template>
-  <div class="footer">Footer</div>
+  <div class="footer">
+    <div class="flex wrap ai-center">
+      <div class="brand-col">
+        <nuxt-link class="footer-brand" to="/">AutoPresent</nuxt-link>
+        <div class="copyright-row">
+          Copyright © 2006-<span>{{ year }}</span
+          >, г. Санкт-Петербург
+        </div>
+      </div>
+      <div class="contacts-col">
+        Телефон: +7 (921) 303-96-06 (г. Cанкт-Петербург),<br />
+        Skype: autopresent,<br />
+        E-mail:<a href="mailto:admin@autopresent.ru">admin@autopresent.ru</a>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      year: new Date().getFullYear(),
+    };
+  },
+};
 </script>
